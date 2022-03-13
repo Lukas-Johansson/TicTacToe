3d5350c700840b9f45bead9bdc2a6fa1e6506e6a
Lukas-Johansson <lukas.johansson4@elev.ga.ntig.se>
2022-03-13 18:03:55 +0100

    Färdig produkt

--- src/sak.java
+++ src/sak.java
@@ -1,88 +1,149 @@
+
+import java.util.*;
+
 public class sak {
 
-    public static void main(String[] args) {
+    static String[] board;
+    static String turn;
 
-        char[][] Spelplan = new char[3][3];
-        instructionBoard();
-        displayBoard();
-        int spelare1 = 1;
-        int spelare2 = 2;
-    }
+    static String checkWinner()
+    {
+        for (int a = 0; a < 8; a++) {
+            String line = null;
 
-    public static void displayBoard(){
-        System.out.println("| - | - | - |");
-        System.out.println(printBoardRow(0));
-        System.out.println("| - | - | - |");
-        System.out.println(printBoardRow(1));
-        System.out.println("| - | - | - |");
-        System.out.println(printBoardRow(2));
-        System.out.println("| - | - | - |");
-    }
+            switch (a) {
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] + board[6];
+                    break;
+                case 4:
+                    line = board[1] + board[4] + board[7];
+                    break;
+                case 5:
+                    line = board[2] + board[5] + board[8];
+                    break;
+                case 6:
+                    line = board[0] + board[4] + board[8];
+                    break;
+                case 7:
+                    line = board[2] + board[4] + board[6];
+                    break;
+            }
+            //For X winner
+            if (line.equals("XXX")) {
+                return "X";
+            }
 
-    public static void instructionBoard(){
-        System.out.println("| - | - | - |");
-        System.out.println("| 1 | 2 | 3 |");
-        System.out.println("| - | - | - |");
-        System.out.println("| 4 | 5 | 6 |");
-        System.out.println("| - | - | - |");
-        System.out.println("| 7 | 8 | 9 |");
-        System.out.println("| - | - | - |");
-    }
-
-    private String printBoardRow(int row){
-        StringBuilder rowBuilder = new StringBuilder("| ");
-        for(int i=0;i<board[0].length;i++){
-            if(board[row][i] == 0) rowBuilder.append(" ");
-            if(board[row][i] == 1) rowBuilder.append("X");
-            if(board[row][i] == 2) rowBuilder.append("O");
-            rowBuilder.append(" | ");
-        }
-        rowBuilder.deleteCharAt(rowBuilder.lastIndexOf(" "));
-        return rowBuilder.toString();
-    }
-
-    public boolean placePiece(int position, String pieceType){
-        int row = (position-1)/3;
-        int col = (position - (row*3))-1;
-        if(board[row][col] == 0) {
-            if (pieceType.equals("X")) board[row][col] = 1;
-            if (pieceType.equals("O")) board[row][col] = 2;
-            return true;
-        }
-        return false;
-    }
-
-    public boolean placePieceRandomly(String pieceType){
-        int row = new Random().nextInt(3);
-        int col = new Random().nextInt(3);
-        boolean wasPiecePlace = false;
-        while(!wasPiecePlace && !isFull()) {
-            if(board[row][col]==0) {
-                wasPiecePlace=true;
-                if (pieceType.equals("X")) board[row][col] = 1;
-                if (pieceType.equals("O")) board[row][col] = 2;
-            }else{
-                row = new Random().nextInt(3);
-                col = new Random().nextInt(3);
+            // For O winner
+            else if (line.equals("OOO")) {
+                return "O";
             }
         }
-        return wasPiecePlace;
 
-        public boolean isFull(){
-            for(int row=0;row<board.length;row++){
-                for(int col=0;col<board[0].length;col++){
-                    if(board[row][col]==0) return false;
+        for (int a = 0; a < 9; a++) {
+            if (Arrays.asList(board).contains(
+                    String.valueOf(a + 1))) {
+                break;
+            }
+            else if (a == 8) {
+                return "draw";
+            }
+        }
+
+        System.out.println(
+                turn + "'s turn; enter a slot number to place "
+                        + turn + " in:");
+        return null;
+    }
+
+    static void printBoard()
+    {
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                + board[1] + " | " + board[2]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                + board[4] + " | " + board[5]
+                + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                + board[7] + " | " + board[8]
+                + " |");
+        System.out.println("|---|---|---|");
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        board = new String[9];
+        turn = "X";
+        String winner = null;
+
+        for (int a = 0; a < 9; a++) {
+            board[a] = String.valueOf(a + 1);
+        }
+
+        System.out.println("Welcome to 3x3 Tic Tac Toe.");
+        printBoard();
+
+        System.out.println(
+                "X will play first. Enter a slot number to place X in:");
+
+        while (winner == null) {
+            int numInput;
+
+            try {
+                numInput = in.nextInt();
+                if (!(numInput > 0 && numInput <= 9)) {
+                    System.out.println(
+                            "Invalid input; re-enter slot number:");
+                    continue;
                 }
             }
-            return true;
+            catch (InputMismatchException e) {
+                System.out.println(
+                        "Invalid input; re-enter slot number:");
+                continue;
+            }
+
+            if (board[numInput - 1].equals(
+                    String.valueOf(numInput))) {
+                board[numInput - 1] = turn;
+
+                if (turn.equals("X")) {
+                    turn = "O";
+                }
+                else {
+                    turn = "X";
+                }
+
+                printBoard();
+                winner = checkWinner();
+            }
+            else {
+                System.out.println(
+                        "Slot already taken; re-enter slot number:");
+            }
+        }
+
+        if (winner.equalsIgnoreCase("draw")) {
+            System.out.println(
+                    "It's a draw! Thanks for playing.");
+        }
+
+        else {
+            System.out.println(
+                    "Congratulations! " + winner
+                            + "'s have won! Thanks for playing.");
         }
     }
-
-
-
 }
-
-
-
-
-
